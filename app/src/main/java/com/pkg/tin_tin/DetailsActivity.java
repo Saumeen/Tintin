36e17f15b63303ec78eedae5037e3014f9ce8227
saumeen prajapati <psaumeen@gmail.com>
2019-08-11 13:26:54 +0530

    detailsActivity updated

--- app/src/main/java/com/pkg/tin_tin/DetailsActivity.java
+++ app/src/main/java/com/pkg/tin_tin/DetailsActivity.java
@@ -1,37 +1,80 @@
 package com.pkg.tin_tin;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
-
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DetailsActivity extends AppCompatActivity {
 
     EditText selectDate;
     private int mYear, mMonth, mDay;
-    private TextInputEditText name,address,mobileno;
+    private TextInputEditText name, address, mobileno;
     private Button submit;
+    private FirebaseFirestore db;
+    private FirebaseUser firebaseUser;
+    private String TAG = "DetailsActivity";
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseAuth firebaseAuth;
+    private DocumentReference documentReference;
+    private Map<String,Object> maplist = new HashMap<>();
+    private String dataname,dataaddress,datamobilno,databirthday;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
         name = findViewById(R.id.name);
         address = findViewById(R.id.address);
-        mobileno = findViewById(R.id.mobileno);
+        mobileno = findViewById(R.id.mobile);
         selectDate = findViewById(R.id.date);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() == null) {
+            Intent intent = new Intent(DetailsActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth1) {
+                if (firebaseAuth1.getCurrentUser() == null) {
+                    //Toast.makeText(MainActivity.this,"in Listner "+ firebaseAuth1.getCurrentUser().toString(),Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(DetailsActivity.this, LoginActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+
+            }
+        };
+
+        firebaseUser = firebaseAuth.getCurrentUser();
+        db =FirebaseFirestore.getInstance();
         selectDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            dateSelection();
+                dateSelection();
             }
         });
 
@@ -39,15 +82,44 @@ public class DetailsActivity extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(DetailsActivity.this,HomeActivity.class);
+                if(isEmpty(name)){
+                    name.setError("Please Enter Your name");
+                }
+                if(isEmpty(address)){
+                    address.setError("please Enter address");
+                }
+                if(isEmpty(mobileno)){
+                    mobileno.setError("Please Enter Mobile No");
+                }
+                else{
+                db.collection("users").whereEqualTo("Email",firebaseUser.getEmail()).get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if(task.isSuccessful()){
+                                    QuerySnapshot qs = task.getResult();
+                                    List<DocumentSnapshot> list = qs.getDocuments();
+
+                                    updateData(list.get(0).getId());
+                                }
+                            }
+                        });
+
+                Intent intent = new Intent(DetailsActivity.this, HomeActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
             }
+        }
         });
+
     }
 
+    public static boolean isEmpty(TextInputEditText textInputEditText) {
+        String input = textInputEditText.getText().toString().trim();
+        return input.length() == 0;
+    }
 
-    public void dateSelection(){
+    public void dateSelection() {
         final Calendar c = Calendar.getInstance();
         mYear = c.get(Calendar.YEAR);
         mMonth = c.get(Calendar.MONTH);
@@ -61,11 +133,66 @@ public class DetailsActivity extends AppCompatActivity {
                     public void onDateSet(DatePicker view, int year,
                                           int monthOfYear, int dayOfMonth) {
 
-                        selectDate.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/"+ year);
+                        selectDate.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
 
                     }
                 }, mYear, mMonth, mDay);
         datePickerDialog.show();
 
     }
+    public void updateData(String id){
+
+        dataname = name.getText().toString();
+        dataaddress = address.getText().toString();
+        datamobilno = mobileno.getText().toString();
+        databirthday = selectDate.getText().toString();
+
+        maplist.put("Name",dataname);
+        maplist.put("Adddress",dataaddress);
+        maplist.put("MobileNo",datamobilno);
+        maplist.put("birthday",databirthday);
+
+        documentReference = db.collection("users").document(id);
+        documentReference.update(maplist).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d(TAG, "DocumentSnapshot successfully updated!");
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error updating document", e);
+                    }
+                });
+    }
+
+
+//    public void updateData() {
+//        db.collection("users")
+//                .whereEqualTo("Email", FirebaseAuth.getInstance().getCurrentUser().getEmail())
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            QuerySnapshot qs = task.getResult();
+//                            if (qs.isEmpty()) {
+//
+//                                Log.d(TAG,"empty");
+//                                // intent.putExtra("user",firebaseUser);
+//
+//
+//                            } else {
+//                               Log.d(TAG,qs.toString());
+//                            }
+//
+//
+//                        } else {
+//                            Log.d(TAG, "Error getting documents: ", task.getException());
+//                        }
+//                    }
+//                });
+//
+//    }
 }
--- app/src/main/java/com/pkg/tin_tin/HomeActivity.java
+++ app/src/main/java/com/pkg/tin_tin/HomeActivity.java
@@ -2,10 +2,18 @@ package com.pkg.tin_tin;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -14,34 +22,10 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import android.provider.MediaStore;
-import android.util.Log;
-import android.view.View;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.core.view.GravityCompat;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-
-import android.view.MenuItem;
-
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.Menu;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.net.URL;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -181,8 +165,7 @@ public class HomeActivity extends AppCompatActivity
                     // User cancelled the dialog
                 }
             });
-// 3. Get the <code><a href="/reference/android/app/AlertDialog.html">AlertDialog</a></code> from <code><a href="/reference/android/app/AlertDialog.Builder.html#create()">create()</a></code>
-            AlertDialog dialog = builder.create();
+           AlertDialog dialog = builder.create();
             dialog.show();
 
         }
