181cee51d8ebad7251488a2bf0052108360395ca
saumeen prajapati <psaumeen@gmail.com>
2019-08-27 01:44:33 +0530

    27-8

---
+++ app/src/main/java/com/pkg/tin_tin/RequestOrderActivity.java
@@ -0,0 +1,14 @@
+package com.pkg.tin_tin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RequestOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_order);
+    }
+}
